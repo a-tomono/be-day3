dfc920b38dc302720a7df781f33549630d09b7cc
a-tomono <a.tomono@gmail.com>
2019-03-03 17:06:50 +0900

    ans: exam02

--- exam02/exam02.js
+++ exam02/exam02.js
@@ -1,23 +1,38 @@
 const crypto = require("crypto");
-const bitArray = require("node-bitarray");
+const bitwise = require("bitwise");
+const fs = require("fs");
 const entropy = process.argv[2].toString();
+const filePath = process.argv[3].toString();
+
+// load word file
+const words = fs.readFileSync(filePath).toString().trim().split("\r\n");
 
 // byte entropy
-const byteEntropy = Buffer.from(entropy.match(/(.{2})/g).reverse().join(""));
-console.log(byteEntropy);
+const byteEntropy = Buffer.from(entropy, "hex");
 
 // make checksum
 const hashedEntropy = crypto.createHash("sha256").update(entropy).digest("hex");
-const byteHashedEntropy = Buffer.from(hashedEntropy.match(/(.{2})/g).reverse().join(""));
-const checksumLength = Math.floor(byteEntropy.length / 32);
-console.log("mnemonicLength: " + checksumLength);
+const byteHashedEntropy = Buffer.from(hashedEntropy, "hex");
+const checksumLength = Math.floor(bitwise.buffer.read(byteEntropy, 0).length / 32);
+
 const checksum = byteHashedEntropy.slice(0, checksumLength);
 console.log("checksum: ");
 console.log(checksum);
 
 // concat entropy + checksum
 const mnemonicCode = Buffer.concat([byteEntropy, checksum]);
-console.log(mnemonicCode);
+const numberOfWords = Math.floor(bitwise.buffer.read(byteEntropy, 0).length + checksumLength) / 11;
+console.log("number of words: " + numberOfWords);
 
+let mnemonic = [];
+for(var i=0; i<numberOfWords; i+=11){
+    mnemonic.push(words[bitwise.buffer.readUInt(mnemonicCode,i,11)]);
+}
+console.log(mnemonic.join(" "));
 
-console.log(mnemonicCode.readUIntBE(0, 6).toString(16));
+console.log("----debug----");
+console.log("length of entropy(byte): " + entropy.length);
+console.log("length of hashedEntropy(byte): " + hashedEntropy.length);
+console.log("bit length of byteEntropy(bit): " + bitwise.buffer.read(byteEntropy, 0).length);
+console.log("bit length of checksumLength(bit): " + checksumLength);
+console.log(bitwise.buffer.create(bitwise.buffer.read(byteHashedEntropy, 0).slice(0, checksumLength)));
